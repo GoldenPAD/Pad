474255fc68f8c40d4e849777a5b4fda5368d5607
GoldenPAD <alvin122006@gmail.com>
2014-03-07 03:55:56 +0800

    Update Update.php

--- Update.php
+++ Update.php
@@ -1,3 +1,20 @@
-<?php header('content-type: application/json; charset=utf-8');
-$data = array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5); 
-echo $_GET['callback'] . '('.json_encode($data).')';
+<?php
+
+$data = '{}'; // json string
+
+if(array_key_exists('callback', $_GET)){
+
+    header('Content-Type: text/javascript; charset=utf8');
+    header('Access-Control-Allow-Origin: http://www.example.com/');
+    header('Access-Control-Max-Age: 3628800');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+    $callback = $_GET['callback'];
+    echo $callback.'('.$data.');';
+
+}else{
+    // normal JSON string
+    header('Content-Type: application/json; charset=utf8');
+
+    echo $data;
+}
